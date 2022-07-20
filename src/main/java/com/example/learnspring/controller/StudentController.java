db2b7c72f0bdaac2f2434ef4f6b554b6ec48ef13
TranDucDuy_ЧанДыкЗюи <tranduybthv@gmail.com>
2022-07-21 01:01:43 +0300

    features to create and show out students

--- src/main/java/com/example/learnspring/controller/StudentController.java
+++ src/main/java/com/example/learnspring/controller/StudentController.java
@@ -1,14 +1,32 @@
 package com.example.learnspring.controller;
 
+import com.example.learnspring.domain.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("student")
 public class StudentController {
+    @Autowired
+    HttpServletRequest request;
+    @Autowired
+    HttpServletResponse response;
     @GetMapping("create")
     public String create(){
         return "create-student";
+    }
+    @PostMapping("create")
+    public String createPost(){
+        String studentId = request.getParameter("studentId");
+        String studentName = request.getParameter("studentName");
+        Student student = new Student(studentId,studentName);
+        request.setAttribute("student", student);
+        return "detail-student";
     }
 }
--- src/main/webapp/WEB-INF/views/detail-student.jsp
+++ src/main/webapp/WEB-INF/views/detail-student.jsp
@@ -5,13 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create Student</title>
+    <title>Student detail</title>
 </head>
 <body>
-    <form action="create" method="post">
-        Student ID: <input type="text" name="studentId"/> <br/>
-        Name: <input type="text" name="studentName"/> <br/>
-        <button>Create</button>
-    </form>
+    <h1>${student.studentId}</h1>
+    <h1>${student.studentName}</h1>
 </body>
 </html>
