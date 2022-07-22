7edbfb53a243be2f217b197ef09de8972a47d2d7
TranDucDuy_ЧанДыкЗюи <tranduybthv@gmail.com>
2022-07-23 00:56:41 +0300

    CRUD - Done

--- src/main/java/com/example/learnspring/controller/StudentController.java
+++ src/main/java/com/example/learnspring/controller/StudentController.java
@@ -5,9 +5,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Controller
 @RequestMapping("students")
 public class StudentController {
+    //create modelAttribute student
+    @ModelAttribute("students")
+    public List<Student> getStudents(){
+        List<Student> list = new ArrayList<>();
+        list.add(new Student("ST1", "Nguyen Van A"));
+        list.add(new Student("ST2", "Nguyen Van B"));
+        return list;
+    }
+
     @GetMapping("new")
     public String newForm() {
         return "students/new";
@@ -50,5 +64,28 @@ public class StudentController {
     public String search() {
         return "students/search";
     }
+    //@RequestMapping("view-detail")
 
+    @GetMapping("view-detail/{studentId}")
+    public String viewDetail(@PathVariable("studentId") String studentId, Model model) {
+        System.out.println("View detail student ID:"+studentId);
+        Student student = new Student();
+        student.setStudentId(studentId);
+        for(Student s: getStudents()) {
+            if(s.getStudentId().equals(studentId)) {
+                student = s;
+                break;
+            }
+        }
+        model.addAttribute("student",student);
+        return "students/view-detail";
+    }
+    //  @RequestMapping("delete/{studentId}")
+    /*@GetMapping("delete/{studentId}")
+    public String delete(@PathVariable("studentId") String studentId, Model model) {
+        System.out.println("Delete student ID:"+studentId);
+        List<Student> list = new ArrayList();
+        list = Stream.of(getStudents()).filter(s -> ! s.getStudentId().equals(studentId)).collect(Collectors.toCollection(ArrayList::new));;
+        return "redirect:/students/list";
+    }*/
 }
--- src/main/webapp/WEB-INF/views/students/list.jsp
+++ src/main/webapp/WEB-INF/views/students/list.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jstl/core_rt" %>
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,8 +7,29 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>List page</title>
+    <base href="/">
 </head>
 <body>
-    <h1>List student</h1>
+    <h1>List students</h1>
+    <hr>
+    // table>tr>td*3
+    <table>
+        <tr>
+            <td>Student ID</td>
+            <td>Name</td>
+            <td></td>
+        </tr>
+        <c:forEach var="student" items="${students}">
+            <tr>
+                <td>${student.studentId}</td>
+                <td>${student.studentName}</td>
+                <td>
+                    <a href="students/view-detail/${student.studentId}">View</a> |
+                    <a href="students/edit/${student.studentId}">Edit</a> |
+                    <a href="students/delete/${student.studentId}">Delete</a>
+                </td>
+            </tr>
+        </c:forEach>
+    </table>
 </body>
 </html>
